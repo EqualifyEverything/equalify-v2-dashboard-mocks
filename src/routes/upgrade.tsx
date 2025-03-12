3ef42a5a481c16316371e3a94a38114de6e500ef
Ezra Bertuccelli <ezra.bertuccelli@gmail.com>
2025-03-11 18:27:56 -0700

    Upgrade page placeholder

--- src/routes/upgrade.tsx
+++ src/routes/upgrade.tsx
@@ -5,5 +5,34 @@ export const Route = createFileRoute('/upgrade')({
 })
 
 function RouteComponent() {
-  return <div>Hello "/upgrade"!</div>
+  return (
+    <main>
+      <h1>You're out of scans!</h1>
+      <div aria-live="assertive">Upgrade to scan more pages with Equalify.</div>
+      <article>
+        <h2>Upgrade</h2>
+        <section>
+          <h3>Basic Plan</h3>
+          <p>200 monthly scans - $40/month</p>
+          <a href="#">Buy Basic</a>
+        </section>
+        <section>
+          <h3>Pro Plan</h3>
+          <p>2000 monthly scans - $150/month</p>
+          <p>Includes priority support</p>
+          <a href="#">Buy Pro</a>
+        </section>
+        <section>
+          <h3>Enterprise Plan</h3>
+          <p>2000+ monthly scans - Custom Pricing</p>
+          <p>Includes priority support, custom checks, and multi-user access</p>
+          <a href="#">Buy Enterprise</a>
+        </section>
+      </article>
+      {/* <article>
+        <h2>Current User</h2>
+        Logged in as Lucy Greco. <a href="#">Logout</a>.
+      </article> */}
+    </main>
+  )
 }
